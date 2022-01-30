66d8f9b4171810267f1cb7cc3b14223bd234530f
Pablo <botterogandia@gmail.com>
2022-01-30 16:55:02 +0100

    created tab bar

--- ios_app/Travis/Travis/ContentView.swift
+++ ios_app/Travis/Travis/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    TabView {
+        
+        FirstView ()
+            .tabItem {
+                Image(systemName: "chart.bar.xaxis")
+                Text("Inicio")
+            }
+        SecondView ()
+            .tabItem {
+                Image(systemName: "heart")
+                Text("Salud")
+            }
+        ThirdView()
+            .tabItem {
+                Image(systemName: "person.circle.fill")
+                Text("Perfil")
+            }
+            }
+    .accentColor(Color.white)
+    .onAppear {
+        UITabBar.appearance().backgroundColor = UIColor.blue
+        UITabBar.appearance().unselectedItemTintColor = UIColor.white
     }
 }
 
@@ -18,4 +38,32 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct FirstView: View {
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea(edges: .top)
+            Text("First View")
+        }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea(edges: .top)
+            Text("Second View")
+        }
+    }
+}
+
+struct ThirdView: View {
+    var body: some View {
+        ZStack {
+            Color.white.ignoresSafeArea(edges: .top)
+            Text("Yhird View")
+        }
+    }
+}
 }
