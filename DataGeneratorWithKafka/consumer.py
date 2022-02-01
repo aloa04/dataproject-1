71406e5af3461b9825ddad36a56db512725e34f8
Thais <ahitana1@gmail.com>
2022-02-01 23:07:11 +0100

    adding bodyfat

--- DataGeneratorWithKafka/consumer.py
+++ DataGeneratorWithKafka/consumer.py
@@ -43,7 +43,7 @@ connection = mysql.connector.connect(host='192.168.1.141',
                                      password='ZurichDb')
 
 sql_get_user = """SELECT clientId FROM Clients WHERE clientId =%s"""
-sql_insert_user = """INSERT INTO Clients (clientId,clientsName,clientsLastName,age,gender,weight,height,bloodPressureSist,bloodPressureDiast,cholesterol,smoker,drinking,disability,previousPathology,postalCode) Values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+sql_insert_user = """INSERT INTO Clients (clientId,clientsName,clientsLastName,age,gender,weight,height,bodyfat,bloodPressureSist,bloodPressureDiast,cholesterol,smoker,drinking,disability,previousPathology,postalCode) Values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
 sql_insert_coordinates = """INSERT INTO Coordinates (clientsId,transportId,latitude, longitude, createDate) Values (%s,%s,%s,%s,%s)"""
 sql_call_insert_sp = """call InsertUpdateKM(%s,%s,%s,%s);"""
 transport = {'Bike': '1', 'Train': '2', 'Car': '3', 'Walking': '4'}
@@ -57,7 +57,7 @@ for event in consumer:
     user = event_data[1]
     if cursor.rowcount == 0:
         cursor.close()
-        insert_data_tuple = (user["id"], user["name"], user["last_name"], user["age"], user["gender"], user["weight"], user["height"], user["bloodpressure_sist"], user["bloodpressure_diast"],
+        insert_data_tuple = (user["id"], user["name"], user["last_name"], user["age"], user["gender"], user["weight"], user["height"], user["bodyfat"],user["bloodpressure_sist"], user["bloodpressure_diast"],
                              user["cholesterol"], True if user["smoker"] == '1' else False, user["drinking"], True if user["disability"] == '1' else False, True if user["previouspatology"] == '1' else False, user["cp"])
         cursor = connection.cursor(buffered=True)
         cursor.execute(sql_insert_user, insert_data_tuple)
