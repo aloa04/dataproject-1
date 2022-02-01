34080775b744d805a5e3a894e25f6884252cd9c0
Pablo <botterogandia@gmail.com>
2022-02-01 13:11:54 +0100

    ContentView

--- ios_app/Travis/Travis/ContentView.swift
+++ ios_app/Travis/Travis/ContentView.swift
@@ -14,12 +14,28 @@ struct ContentView: View {
             Image("memoji")
                 .resizable()
                 .frame(width: 100.0, height: 110.0)
+                .padding(.top, 25)
             VStack(alignment: .leading) {
                 Text("Bienvenido, Pedro")
                     .font(.system(size:32, weight: .medium, design: .default))
+                    .padding(.top, 25)
                 Text("Miembro desde 2022")
             }
         }
+            VStack {
+                Text("División (3 iconos, uno de ellos con mayor tamaño")
+                    .padding(.bottom, 25)
+                Text("74 puntos")
+                    .padding(.bottom, 25)
+                Text("KM andados + 34% de la media")
+                    .padding(.bottom, 25)
+                Text("Mapa de la ruta hecha andando")
+                    .padding(.bottom, 25)
+                Text("KM en cleta + 2% de la media")
+                    .padding(.bottom, 25)
+                Text("Mapa de la ruta hecha en cleta")
+                    .padding(.bottom, 25)
+            }
             Spacer()
     }
 }
